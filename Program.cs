f5174b84368ee668a3ddab635b7788f8b8d31777
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-21 07:29:02 -0700

    updates

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+
+var videogames= new List<string>() {"Halo","Mario Kart", "Mortal Combat", "Pokemon","GTA", "Elden Ring" };
+
+//videogames.OrderBy(games => games).ToList().ForEach(game => Console.WriteLine(game));
+
+videogames.OrderByDescending(games=> games.Length).ToList().ForEach(game => Console.WriteLine(game));
